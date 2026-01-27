6b0164713f0f7ec2128bcf69950cba65aea07390
AziAzi22 <aziazi22t@gmail.com>
2026-01-27 17:50:17 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+// interface MyFunction {
+//     (arr: number[]): number;
+// }
+
+// function add(arr: number[]): number {
+//   const sum = arr.reduce((a, b) => a + b, 0);
+
+//   return sum;
+// }
+
+// console.log(add([1, 2, 3]));
+
+
+
+
+// interface MyFunction {
+//   (arr: string[]): number[];
+// }
+
+// const get_length: MyFunction = (arr) => {
+//   let res = arr.map((str) => str.length);
+//   return res;
+// };
+// console.log(get_length(["a", "ab", "abc", "abcd"]));
+
+
+let user : [string, number, string] = ["ali", 33, "footballplayer"]; 
+console.log(user);
+
+
+
+
